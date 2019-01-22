f4daa5f8a5a1280b791acd1b7b2f08937a8e8468
Luke Secomb <lukessecomb@gmail.com>
2019-01-23 00:02:06 +1100

    fixed move + scale func / added basic bg_img switching / added images / removed audio files
    fixed move + scale func
    added basic bg_img switching (needs fixing)
    added images
    removed audio files

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 
+var wallys = [
+  {
+    'name': 'The Unfriendly Giants',
+    'source': 'the-unfriendly-giants.jpg'
+  },
+  {
+    'name': 'Smmm! This is a Silent Movie',
+    'source': 'smmm-this-is-a-silent-movie.jpg'
+  },
+  {
+    'name': 'Robin Hoods Merry Mess Up',
+    'source': 'robin-hoods-merry-mess-up.jpg'
+  },
+  {
+    'name': 'The Knights of the Magic Flag',
+    'source': 'the-knights-of-the-magic-flag.jpg'
+  },
+  {
+    'name': 'Zalando Festival',
+    'source': 'zalando-festival-map.jpg'
+  }
+];
+
+// wallys.forEach(element => {
+//   console.log('element', element);
+//   var thumbnail_display = document.getElementById('thumbnail_display');
+
+//   var item_div = document.createElement("div");
+  
+//   var item_name = document.createElement("h3");
+//   item_name.appendChild(document.createTextNode("name: " + element.name));
+//   item_div.appendChild(item_name);
+
+//   thumbnail_display.appendChild(item_div);
+// });
+
+
 var haveEvents = 'GamepadEvent' in window;
 var haveWebkitEvents = 'WebKitGamepadEvent' in window;
 var controllers = {};
+var bg_index = 0;
 var rAF = window.mozRequestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.requestAnimationFrame;
+
+
+var btn_14_timestamp = new Date();
+var btn_15_timestamp = new Date();
 
 function connecthandler(e) {
   addgamepad(e.gamepad);
@@ -35,7 +77,7 @@ function addgamepad(gamepad) {
     e = document.createElement("meter");
     e.className = "axis";
     //e.id = "a" + i;
-    // var leftMargin = document.createElement("select");
+    // var rightMargin = document.createElement("select");
     e.setAttribute("min", "-1");
     e.setAttribute("max", "1");
     e.setAttribute("value", "0");
@@ -87,12 +129,12 @@ function updateStatus() {
       currentY = 0.1;
     }
     var controllerAxixX = Math.round(controller.axes[0] * 8) * 2;
-    console.log('controllerAxixX', controllerAxixX)
+    // console.log('controllerAxixX', controllerAxixX)
     var controllerAxixY = Math.round(controller.axes[1] * 8) * 2;
 
-    var elementAxisX = Math.round( Number(bg.style.left.slice(0, -2)));
-    console.log('elementAxisX', elementAxisX)
-    var elementAxisY = Math.round( Number(bg.style.top.slice(0, -2)));
+    var elementAxisX = Math.round( Number(bg.style.right.slice(0, -2)));
+    // console.log('elementAxisX', elementAxisX)
+    var elementAxisY = Math.round( Number(bg.style.bottom.slice(0, -2)));
 
     if (elementAxisX === 0) {
       elementAxisX = 10
@@ -103,26 +145,103 @@ function updateStatus() {
 
     var elementScale = bg.getAttribute('valueS');
 
+    // CHANGE BG IMAGE 
+
+    // console.log('Math.round(btn_14_timestamp * 10)', Math.round(btn_14_timestamp.getSeconds() * 100) / 10);
+    // console.log('Math.round(new Date().getSeconds * 10)', Math.round(new Date().getSeconds() * 100) / 10);
+
+    if ( (Math.round(btn_14_timestamp.getSeconds()) / 10) !== (Math.round(new Date().getSeconds()) / 10)) {
+      console.log('btn_14_timestamp', btn_14_timestamp.getSeconds() * 10)
+      btn_14_timestamp = new Date();
+
+      // if (controller.buttons[14].pressed) {
+      //   console.log('btn 4 - bg_index', bg_index)
+        
+
+      //   if (bg_index !== 0) {
+      //     bg_index = bg_index - 1;
+      //     var img = document.getElementById("background_image")
+      //     img.src = './wallys/' + wallys[bg_index].source;
+      //     console.log('./wallys/' + wallys[bg_index].source)
+      //     btn_14_timestamp = new Date().getSeconds;
+      //   }
+      // }
+    } else {
+      if (controller.buttons[14].pressed) {
+        console.log('btn 4 - bg_index', bg_index)
+        
+
+        if (bg_index !== 0) {
+          bg_index = bg_index - 1;
+          var img = document.getElementById("background_image")
+          img.src = './wallys/' + wallys[bg_index].source;
+          console.log('./wallys/' + wallys[bg_index].source)
+          btn_14_timestamp = new Date().getSeconds;
+        }
+      }
+    }
+    if (Math.round(btn_15_timestamp * 100) === Math.round(new Date().getSeconds * 100)) {
+      console.log('btn_15_timestamp', btn_15_timestamp)
+      if (controller.buttons[15].pressed) {
+        console.log('btn 5 - bg_index', bg_index)
+        if (bg_index !== (wallys.length - 1)) {
+          bg_index = bg_index + 1;
+          var img = document.getElementById("background_image")
+          img.src = './wallys/' + wallys[bg_index].source;
+          console.log('./wallys/' + wallys[bg_index].source)
+          btn_15_timestamp = new Date().getSeconds;
+        }
+      }
+    }
+
     // SCALE
     if (controller.buttons[6].pressed) {
       console.log('btn pressed 6', controller.buttons)
       var currentScale = Number(bg.style.transform.slice(6, -1));
-      if (currentScale === 0) {
-        currentScale = 0.01;
+      console.log('currentScale', currentScale)
+      if (currentScale === 1) {
+        currentScale = 0.95;
       }
-      console.log('Number(bg.style.transform.slice(6, -1))', currentScale);
-      bg.style.transform = "scale(" + (currentScale - 0.01) + ')';
-      bg.setAttribute("valueS", currentScale);
+      if (currentScale === 0) {
+        currentScale = 0.05;
+      }
+
+      var newScale = Number(elementScale) - 0.01;
+      
+      console.log('currentScale', currentScale);
+      console.log('newScale', newScale);
+      bg.style.transform = "scale(" + newScale + ')';
+      bg.setAttribute("valueS", newScale);
     }
     if (controller.buttons[7].pressed) {
-      // console.log('btn pressed 7', controller.buttons)
+
+      console.log('btn pressed 7', controller.buttons)
       var currentScale = Number(bg.style.transform.slice(6, -1));
-      if (currentScale === 0) {
-        currentScale = 0.01;
+      console.log('currentScale', currentScale)
+      if (currentScale === 1) {
+        currentScale = 0.95;
       }
-      console.log('Number(bg.style.transform.slice(6, -1))', currentScale);
-      bg.style.transform = "scale(" + (currentScale + 0.01) + ')';
-      bg.setAttribute("valueS", currentScale);
+      if (currentScale === 0) {
+        currentScale = 0.05;
+      }
+
+      var newScale = Number(elementScale) + 0.01;
+      
+      console.log('currentScale', currentScale);
+      console.log('newScale', newScale);
+      bg.style.transform = "scale(" + newScale + ')';
+      bg.setAttribute("valueS", newScale);
+
+
+      // OLD //
+      // console.log('btn pressed 7', controller.buttons)
+      // var currentScale = Number(bg.style.transform.slice(6, -1));
+      // if (currentScale === 0) {
+      //   currentScale = 0.01;
+      // }
+      // // console.log('Number(bg.style.transform.slice(6, -1))', currentScale);
+      // bg.style.transform = "scale(" + (currentScale + 0.01) + ')';
+      // bg.setAttribute("valueS", currentScale);
     }
 
     // checks if current element position and new value from controller match and the apply new style according
@@ -133,7 +252,7 @@ function updateStatus() {
       bg.setAttribute("valueX", newXVal + 'px');
 
       // get new value and updates element style
-      bg.style.left = bg.getAttribute("valueX");
+      bg.style.right = bg.getAttribute("valueX");
       
     } else {
       console.warn('DOES NOT EQUAL')
@@ -147,7 +266,7 @@ function updateStatus() {
       bg.setAttribute("valueY", newYVal + 'px');
 
       // get new value and updates element style
-      bg.style.top = bg.getAttribute("valueY");
+      bg.style.bottom = bg.getAttribute("valueY");
       
     } else {
       console.warn('DOES NOT EQUAL')
